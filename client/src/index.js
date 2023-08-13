867bc9ee2f15edd3978c6d044b0e4bf0e8b5f861
Michael Nabatov <happyanddebtfree@gmail.com>
2023-08-13 15:14:55 -0400

    Added material ui theme provider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { ThemeProvider, createTheme } from "@mui/material";
+import { orange, blue } from "@mui/material/colors";
 import App from './App';
 
+const theme = createTheme({
+  pallete: {
+    primary: {
+      main: blue[500]
+    },
+    secondary: {
+      main: orange[500]
+    }
+  }
+});
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+
+
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+  );
 
 
